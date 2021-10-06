da28ec445f79096dd3c0211a797fabb7e434caab
freshwlnd <985605915@qq.com>
2021-10-06 13:44:34 +0800

    completed HM2 script

---
+++ HW2/mapper.py
@@ -0,0 +1,9 @@
+# mapper.py
+
+import sys
+
+for line in sys.stdin:
+	line = line.strip()
+	words = line.split()
+	for word in words:
+		print('{}\t{}'.format(word,1))
---
+++ HW2/reducer.py
@@ -0,0 +1,26 @@
+# reducer.py
+
+import sys
+
+current_word = None
+current_count = 0
+word = None
+
+for line in sys.stdin:
+	line = line.strip()
+	word, count = line.split('\t',1)
+	try:
+		count = int(count)
+	except ValueError:
+		continue
+
+	if current_word == word:
+		current_count += count
+	else:
+		if current_word is not None:
+			print('{}\t{}'.format(current_word,current_count))
+		current_word = word
+		current_count = count
+
+if current_word is not None:
+	print('{}\t{}'.format(current_word,current_count))
